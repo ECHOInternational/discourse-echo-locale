b84fa8b50d09cb1cb25a89511743e5d4c3c63af7
Nate Flood <nate@famconnect.net>
2017-03-28 10:33:33 -0400

    match locales to ECHOcommunity

--- plugin.rb
+++ plugin.rb
@@ -9,14 +9,33 @@ register_asset "stylesheets/hacks.css"
 gem "http_accept_language", "2.0.5"
 
 after_initialize do
+	# match the supported locales to the main site.
+	LocaleSiteSetting.class_eval do
+	  def self.supported_locales
+	    %w(en es fr my vi th zh zh_CN sw id km)
+	  end
+	end
+
 	ApplicationController.class_eval do 
 		def set_locale
-			begin	
-				I18n.locale = params[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+			begin
+				detected_locale = params[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+				I18n.locale = map_locale(detected_locale)
 			rescue I18n::InvalidLocale
 				I18n.locale = I18n.default_locale
 			end
 			I18n.ensure_all_loaded!
+		end
+
+		def map_locale(locale)
+			case locale.to_s
+			when "en", "es", "fr", "vi", "id"
+				locale
+			when "zh"
+				"zh_CN"
+			when "my", "th", "sw", "km"
+				"en"
+			end
 		end
 
 		def default_url_options(options={})
